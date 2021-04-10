3304679bcaeccd9b48362e4c6e0e4b85fc02d685
Laurie Marceau <laurie.marceau@intact.net>
2021-04-10 12:01:49 -0400

    Swiftlint autocorrect ran on the project

--- AppForBudget/AppForBudget/ViewViewController.swift
+++ AppForBudget/AppForBudget/ViewViewController.swift
@@ -11,41 +11,37 @@ class ViewViewController: UIViewController {
 
     public var item: ToDoListItem?
     public var deletionHandler: (() -> Void)?
-    
+
     @IBOutlet var itemLabel: UILabel!
     @IBOutlet var costLabel: UILabel!
-    
+
     private let realm = try! Realm()
-    
+
     static let dateFormatter: DateFormatter = {
-       let dateFormatter = DateFormatter()
+        let dateFormatter = DateFormatter()
         dateFormatter.dateStyle = .medium
         return dateFormatter
     }()
-    
+
     override func viewDidLoad() {
         super.viewDidLoad()
 
         itemLabel.text = item?.item
         costLabel.text = Self.dateFormatter.string(from: item!.date )
-        
+
         navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .trash, target: self, action: #selector(didTapDelete))
     }
-    
+
     @objc private func didTapDelete() {
         guard let myItem = self.item else {
             return
         }
- 
+
         realm.beginWrite()
         realm.delete(myItem)
         try! realm.commitWrite()
-        
+
         deletionHandler?()
         navigationController?.popToRootViewController(animated: true)
-    
     }
- 
-
 }
-  
